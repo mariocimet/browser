fc52f6b4d6ee2fb5ccc9713236c0e416d8b196e8
Mario Cimet <mario@brave.coop>
2022-02-08 09:45:19 -0800

    Adding support for gzip compression and chunked data transfer

--- browser.py
+++ browser.py
@@ -1,110 +1,137 @@
 import socket
 import ssl
+import gzip
+import pdb
 
 URL_SCHEMES = ["http", "https", "file", "data", "view-source"]
-SUPPORTED_ENTITIES = {"&lt;":"<", "&gt;":">", "&amp;":"&"}
+SUPPORTED_ENTITIES = {"&lt;": "<", "&gt;": ">"}
 
 def add_headers(request, headers):
     for key, val in headers.items():
-        request += f'\r\n{key}: {val}'
-    return request + '\r\n\r\n'
+        request += f"\r\n{key}: {val}"
+    return request + "\r\n\r\n"
+
 
 def print_entity(entity):
     if entity in SUPPORTED_ENTITIES:
-        print(SUPPORTED_ENTITIES.get(entity), end="")    
+        print(SUPPORTED_ENTITIES.get(entity), end="")
 
 
 def transform(source_code):
     for entity, reserved_char in SUPPORTED_ENTITIES.items():
         source_code = source_code.replace(reserved_char, entity)
-    return f'<body>{source_code}</body>'
+    return f"<body>{source_code}</body>"
+
+    
 
 def request(url):
     if not url:
-        return {}, open('/home/mario/browser/browser.py', 'r')
+        return {}, open("/home/mario/browser/browser.py", "r")
 
-        
     # Use socket to connect to the host, implementation of https://en.wikipedia.org/wiki/Berkeley_sockets
-        # AF = Address family, because it's a web browser we use INET6 instead of, say UNIX or BLUETOOTH.
-        # Note: INET6 is compatible w/ IPv6 and backwards-compatible w/ IPv4
-        # STREAM=Streaming, allows arbitrary data to be sent in a, well, stream. Common alternative is SOCK_DGRAM which requires packets of a specific size
-        # RAW, RDM, and SEQPACKET are also allowed but are very rare.
-        # IPPROTO = which protocol to use, TCP is the most common (the handshakey one). Alternatives include UDP or Google's implementation of it, QUIC 
-        # These are all defaults so this block is the equivalent of calling socket.socket()
-    
+    # AF = Address family, because it's a web browser we use INET6 instead of, say UNIX or BLUETOOTH.
+    # Note: INET6 is compatible w/ IPv6 and backwards-compatible w/ IPv4
+    # STREAM=Streaming, allows arbitrary data to be sent in a, well, stream. Common alternative is SOCK_DGRAM which requires packets of a specific size
+    # RAW, RDM, and SEQPACKET are also allowed but are very rare.
+    # IPPROTO = which protocol to use, TCP is the most common (the handshakey one). Alternatives include UDP or Google's implementation of it, QUIC
+    # These are all defaults so this block is the equivalent of calling socket.socket()
     s = socket.socket(
-    family=socket.AF_INET,
-    type=socket.SOCK_STREAM,
-    proto=socket.IPPROTO_TCP,
-)
-
-    # Split the URL into parts 
-        # Scheme, describes how to retrieve the resource
-        # Host and path describe where to get it and what to get from it
-        # Port specifies the connection interface to use
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
+    )
+    # Split the URL into parts
+    # Scheme, describes how to retrieve the resource
+    # Host and path describe where to get it and what to get from it
+    # Port specifies the connection interface to use
     scheme, url = url.split(":", 1)
 
-    assert scheme in URL_SCHEMES, \
-    "Unknown scheme {}".format(scheme)
+    assert scheme in URL_SCHEMES, "Unknown scheme {}".format(scheme)
+
+    if scheme == "view-source":
+        subscheme, url = url.split(":")
 
     if scheme != "data":
-        print(url)
         prefix, url = url.split("//", 1)
-
-    print(url)
 
     host, path = url.split("/", 1)
 
     if ":" in host:
-      host, port = host.split(":", 1)
-      port = int(port)
-    
+        host, port = host.split(":", 1)
+        port = int(port)
+
     path = "/" + path
     port = 80 if scheme == "http" else 443
 
     # wrap the socket with a ssl context if we're using https
-    if scheme == "https":
-      ctx = ssl.create_default_context()
-      s = ctx.wrap_socket(s, server_hostname=host)
+    if scheme or subscheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=host)
 
     if scheme == "file":
-        return {}, open(url, 'r')
+        return {}, open(url, "r")
 
     if scheme == "data":
         encoding, content = path.split(",", 1)
         return {}, content
-        
+
     s.connect((host, port))
 
-    request = f'GET {path} HTTP/1.1'
-    headers = {"Host":host, "Connection":"close", "User-Agent":"mcdat"}
+    request = f"GET {path} HTTP/1.1"
+    headers = {
+        "Host": host,
+        "Connection": "close",
+        "User-Agent": "mcdat",
+        "Accept-Encoding": "gzip",
+    }
     requestWithHeaders = add_headers(request, headers)
-    
+
     # need to send the data in binary, which means encoding: https://www.python.org/dev/peps/pep-0498/#no-binary-f-strings
-    s.send(requestWithHeaders.encode("latin-1"))
+    s.send(requestWithHeaders.encode("utf-8"))
 
     # socket.makefile returns the whole response associated with the socket (otherwise we'd need to loop over the response to parse it)
-    response = s.makefile("r", encoding="latin-1", newline="\r\n")
-    statusline = response.readline()
+    response = s.makefile("b", newline="\r\n")
+
+    statusline = response.readline().decode("utf-8")
     version, status, explanation = statusline.split(" ", 2)
 
     # assert OK status, throw an error for non 200 return codes (300s for redirects, 400 for client errors, 500 for server errors etc.)
     assert status == "200", "{}: {}".format(status, explanation)
     # Parse headers
+
     headers = {}
     while True:
-        line = response.readline()
-        if line == "\r\n": break
+        line = response.readline().decode("utf-8")
+        if line == "\r\n":
+            break
         header, value = line.split(":", 1)
         headers[header.lower()] = value.strip()
 
-    body = response.read()
+    if "transfer-encoding" in headers and headers["transfer-encoding"] == "chunked":
+        lines = response.read().split(b'\r\n')
+        pdb.set_trace()
+        body = ''
+        for index, line in enumerate(lines):
+            if index % 2 == 0:
+                continue
+            else:
+                body += gzip.decompress(line).decode("utf-8")
+
+    if headers["content-encoding"] != "gzip":
+        body = response.read().decode("utf-8")
+
+    # if headers["transfer-encoding"] == "chunked":
+    #     pdb.set_trace()
+
+    # body = gzip.decompress(response.read()).decode("utf-8")
+
     s.close()
 
     if scheme == "view-source":
-        return headers, transform(body)
+        body = transform(body)
 
     return headers, body
+
 
 # Initial parser state machine to print just text, not tags, from an html page.
 def show(source_text):
@@ -131,7 +158,7 @@ def show(source_text):
             continue
 
         if not in_body:
-            continue   
+            continue
 
         if c == "&":
             in_entity = True
@@ -139,14 +166,15 @@ def show(source_text):
             continue
         elif in_entity and c == ";":
             in_entity = False
-            entity +=c
+            entity += c
             print_entity(entity)
             entity = ""
             continue
         elif in_entity:
-            entity +=c
+            entity += c
         if not in_entity:
-             print(c, end="")
+            print(c, end="")
+
 
 def load(url):
     headers, body = request(url)
@@ -155,10 +183,10 @@ def load(url):
 
 if __name__ == "__main__":
     import sys
+
     if len(sys.argv) == 1:
         load("")
     elif len(sys.argv) == 2:
         load(sys.argv[1])
     else:
-        raise Exception(f'Too many arguments. Expected 1, given {len(sys.argv) -1}')
-    
+        raise Exception(f"Too many arguments. Expected 1, given {len(sys.argv) -1}")
